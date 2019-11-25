0c4e791cf9de162e28aa74953e24cecbac1b4e7e
samsung <million.fil@mail.ru>
2019-11-25 14:37:50 +0500

    new sites to yandex

--- src/main/java/ru/fil/yandex/Main.java
+++ src/main/java/ru/fil/yandex/Main.java
@@ -14,26 +14,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Main {
+
+    private static WebDriver driver;
+
     public static void main(String[] args) throws InterruptedException {
         FileInputStream inputXlsx;
         FileOutputStream outXlsx;
         XSSFWorkbook res;
         XSSFSheet sheet;
-        int lastRow, lastcell,i=0;
+        int lastRow, lastcell, i = 0;
         Row row;
+        List<String> sites = new ArrayList();
+        sites.add("https://yandex.ru/maps/50/perm/?ll=56.174791%2C57.981587&mode=routes&rtext=57.961349%2C56.172858~57.971996%2C56.181868~58.008390%2C56.188022&rtt=auto&ruri=ymapsbm1%3A%2F%2Fgeo%3Fll%3D56.173%252C57.961%26spn%3D0.001%252C0.001%26text%3D%25D0%25A0%25D0%25BE%25D1%2581%25D1%2581%25D0%25B8%25D1%258F%252C%2520%25D0%259F%25D0%25B5%25D1%2580%25D0%25BC%25D1%258C%252C%2520%25D1%2583%25D0%25BB%25D0%25B8%25D1%2586%25D0%25B0%2520%25D0%2590%25D1%2580%25D1%2585%25D0%25B8%25D1%2582%25D0%25B5%25D0%25BA%25D1%2582%25D0%25BE%25D1%2580%25D0%25B0%2520%25D0%25A1%25D0%25B2%25D0%25B8%25D1%258F%25D0%25B7%25D0%25B5%25D0%25B2%25D0%25B0%252C%252046%252F2~~ymapsbm1%3A%2F%2Fgeo%3Fll%3D56.188%252C58.008%26spn%3D0.001%252C0.001%26text%3D%25D0%25A0%25D0%25BE%25D1%2581%25D1%2581%25D0%25B8%25D1%258F%252C%2520%25D0%259F%25D0%25B5%25D1%2580%25D0%25BC%25D1%258C%252C%2520%25D1%2583%25D0%25BB%25D0%25B8%25D1%2586%25D0%25B0%2520%25D0%2591%25D1%2583%25D0%25BA%25D0%25B8%25D1%2580%25D0%25B5%25D0%25B2%25D0%25B0%252C%252015&z=12");
+        sites.add("https://yandex.ru/maps/50/perm/?ll=56.238265%2C57.998537&mode=routes&rtext=57.986516%2C56.291370~58.011432%2C56.237665~58.008390%2C56.188022&rtt=auto&ruri=~~ymapsbm1%3A%2F%2Fgeo%3Fll%3D56.188%252C58.008%26spn%3D0.001%252C0.001%26text%3D%25D0%25A0%25D0%25BE%25D1%2581%25D1%2581%25D0%25B8%25D1%258F%252C%2520%25D0%259F%25D0%25B5%25D1%2580%25D0%25BC%25D1%258C%252C%2520%25D1%2583%25D0%25BB%25D0%25B8%25D1%2586%25D0%25B0%2520%25D0%2591%25D1%2583%25D0%25BA%25D0%25B8%25D1%2580%25D0%25B5%25D0%25B2%25D0%25B0%252C%252015&z=13");
+        sites.add("https://yandex.ru/maps/50/perm/?ll=56.248786%2C58.002883&mode=routes&rtext=58.033940%2C56.324029~58.011432%2C56.237665~58.008390%2C56.188022&rtt=auto&ruri=~~ymapsbm1%3A%2F%2Fgeo%3Fll%3D56.188%252C58.008%26spn%3D0.001%252C0.001%26text%3D%25D0%25A0%25D0%25BE%25D1%2581%25D1%2581%25D0%25B8%25D1%258F%252C%2520%25D0%259F%25D0%25B5%25D1%2580%25D0%25BC%25D1%258C%252C%2520%25D1%2583%25D0%25BB%25D0%25B8%25D1%2586%25D0%25B0%2520%25D0%2591%25D1%2583%25D0%25BA%25D0%25B8%25D1%2580%25D0%25B5%25D0%25B2%25D0%25B0%252C%252015&z=13");
         List<WebElement> webElements;
         WebElement webElement;
-        WebDriver driver;
         ChromeOptions opt = new ChromeOptions();
         opt.addArguments("headless");
-        boolean rain = false;
         while (true) {
             System.setProperty("webdriver.chrome.driver", "/home/samsung/chromedriver");
             driver = new ChromeDriver(opt);
@@ -45,75 +54,27 @@ public class Main {
                 sheet = res.getSheetAt(0);
                 lastRow = sheet.getLastRowNum();
                 row = sheet.createRow(lastRow + 1);
-                boolean rain = false;
                 String timeStamp = new SimpleDateFormat("MM.dd.yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
                 row.createCell(lastcell).setCellValue(timeStamp);
                 System.out.println(timeStamp);
                 driver.get("https://yandex.ru");
-                webElement = driver.findElement(By.className("weather__temp"));
+                webElement = fluentWait(By.className("weather__temp"));
                 row.createCell(++lastcell).setCellValue(webElement.getText().substring(0, webElement.getText().length() - 1));
                 System.out.println(webElement.getText());
                 driver.get("https://yandex.ru/pogoda/perm");
-                webElement = driver.findElement(By.className("day-anchor"));
+                webElement = fluentWait(By.className("day-anchor"));
                 if (webElement.getText().contains("дождь") || webElement.getText().contains("снег")) {
-                    rain = true;
-                }
-                if (rain) {
                     row.createCell(++lastcell).setCellValue(1);
                 } else {
                     row.createCell(++lastcell).setCellValue(0);
                 }
-                driver.get("https://www.google.ru/maps/dir/57.9614163,56.1745576/58.0087631,56.1879164/@57.9847595,56.138158,13z/am=t/data=!3m1!4b1!4m3!4m2!3e0!5i1");
-                Thread.sleep(1000);
-                webElements = driver.findElements(By.className("day-anchor"));
-                i=0;
-                while (webElements.size() == 0 || i != 10) {
-                    webElements = driver.findElements(By.className("delay-light"));
-                    i++;
-                }
-                if(webElements.get(0).getText().split(" ")[0].length()==0)
-                {
-                    System.out.println(1);
+                for (String site : sites) {
+                    driver.get(site);
                     Thread.sleep(1000);
-//                    System.out.println(webElements.get(0).getText());
-//                    System.out.println(webElements.get(1).getText());
+                    webElement = fluentWait(By.className("auto-route-form-view__route-title-primary"));
+                    row.createCell(++lastcell).setCellValue(webElement.getText().split(" ")[0]);
+                    System.out.println(webElement.getText());
                 }
-                row.createCell(++lastcell).setCellValue(webElements.get(0).getText().split(" ")[0]);
-                System.out.println(webElements.get(0).getText());
-                driver.get("https://www.google.ru/maps/dir/57.986497,56.2914518/58.0087631,56.1879164/@58.0014633,56.1731455,12z/am=t/data=!3m1!4b1!4m3!4m2!3e0!5i1");
-                Thread.sleep(1000);
-                webElements = driver.findElements(By.className("delay-light"));
-                i=0;
-                while (webElements.size() == 0 || i!=10) {
-                    webElements = driver.findElements(By.className("delay-light"));
-                    i++;
-                }
-                if(webElements.get(0).getText().split(" ")[0].length()==0)
-                {
-                    System.out.println(2);
-                    Thread.sleep(1000);
-//                    System.out.println(webElements.get(0).getText());
-//                    System.out.println(webElements.get(1).getText());
-                }
-                row.createCell(++lastcell).setCellValue(webElements.get(0).getText().split(" ")[0]);
-                System.out.println(webElements.get(0).getText());
-                driver.get("https://www.google.ru/maps/dir/58.0338697,56.3240669/58.0087631,56.1879164/@58.0209894,56.1854751,12z/am=t/data=!3m1!4b1!4m3!4m2!3e0!5i1");
-                Thread.sleep(1000);
-                webElements = driver.findElements(By.className("delay-light"));
-                i = 0;
-                while (webElements.size() == 0||i!=10) {
-                    webElements = driver.findElements(By.className("delay-light"));
-                    i++;
-                }
-                if(webElements.get(0).getText().split(" ")[0].length()==0)
-                {
-                    System.out.println(3);
-                    Thread.sleep(1000);
-//                    System.out.println(webElements.get(0).getText());
-//                    System.out.println(webElements.get(1).getText());
-                }
-                row.createCell(++lastcell).setCellValue(webElements.get(0).getText().split(" ")[0]);
-                System.out.println(webElements.get(0).getText());
                 inputXlsx.close();
                 outXlsx = new FileOutputStream("res.xlsx");
                 res.write(outXlsx);
@@ -125,7 +86,24 @@ public class Main {
                 driver.close();
             }
             System.out.println("Program sleep : 1 h");
-            Thread.sleep(1000);
+            Thread.sleep(1000 * 60 * 60);
         }
     }
+
+    public static WebElement fluentWait(final By locator) {
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(50, TimeUnit.SECONDS)
+                .pollingEvery(10, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElements(locator).get(0);
+            }
+        });
+
+        return foo;
+    }
+
+    ;
 }
